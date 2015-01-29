8804c2b1cb0e010233d3a17cda09eac3488f802a
Jennifer Duvall <jen.k.falk@gmail.com>
2015-01-29 18:07:41 -0500

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'pages/about'
 
-    root 'welcome#index'
+    root 'pages/landing_page'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
