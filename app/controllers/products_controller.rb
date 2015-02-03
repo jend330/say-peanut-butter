3c0760c67665f061778c881a85e65426604e901c
Jennifer Duvall <jen.k.falk@gmail.com>
2015-02-02 19:44:15 -0500

    created order pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -75,3 +75,4 @@ end
 def product_params
   params.require(:product).permit(:name, :description, :image_url, :colour)
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'pages/about'
 
     root 'pages#landing_page'
+
+    resources :orders, only: [:index, :show, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
