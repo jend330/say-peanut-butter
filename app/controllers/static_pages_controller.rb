31609c0c644f380f1273293c518373a5b417e8b7
Jennifer Duvall <jen.k.falk@gmail.com>
2015-02-11 17:14:42 -0500

    edit controllers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,11 @@ class StaticPagesController < ApplicationController
 		@featured_product = Product.first
 		@products = Product.all
 	end
+	def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'jduvall330@gmail.com', :subject => "A new contact form message from #{@name}", :body => @message).deliver
+  end
 end
