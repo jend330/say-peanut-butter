fb82d59bb5828db9e353f4b2128729d9874143c1
Jennifer Duvall <jen.k.falk@gmail.com>
2015-02-12 19:57:47 -0500

    devise user install and edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'say-peanut-butter.herokuapp.com'}
 end
