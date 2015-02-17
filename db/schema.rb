523c23e3428a320a421db84b9671bcd70b0ff896
Jennifer Duvall <jduvall330@gmail.com>
2015-02-17 17:38:27 -0500

    removed ability.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,11 @@
 ActiveRecord::Schema.define(version: 20150217161301) do
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "product_id"
     t.float   "total"
   end
 
   add_index "orders", ["product_id"], name: "index_orders_on_product_id"
-  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
