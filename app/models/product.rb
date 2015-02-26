1d353c28f324b7a83982cc85e46fdc9d19b707a7
Jennifer Duvall <jduvall330@gmail.com>
2015-02-26 15:53:30 -0500

    edited tests for FactoryGirl

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
+	validates :name, presence:true
 	attr_accessible :name, :description, :image_url
 end
--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -8,8 +8,9 @@ describe ProductsController do
 
 	describe 'GET #index' do
 		it "populates an array of all products" do
-			test1 = Product.create(name: 'test1')
-			test2 = Product.create(name: 'test2')
+			Product.destroy_all
+			test1 = FactoryGirl.create(:product, name: 'test1')
+			test2 = FactoryGirl.create(:product, name: 'test2')
 			get :index
 			expect(assigns(:products)).to match_array([test1, test2])
 		end
@@ -19,15 +20,15 @@ describe ProductsController do
 			expect(response).to render_template :index
 		end 
 	end
-		
+
 	describe 'GET #show' do
 		it "assigns the requested product to @product" do
-			product = Product.create
+			product = FactoryGirl.create(:product)
 			get :show, id: product
 			expect(assigns(:product)).to eq product
 		end
 		it "renders the :show template" do
-			product = Product.create
+			product = FactoryGirl.create(:product)
 			get :show, id: product
 			expect(response).to render_template :show
 		end
@@ -35,12 +36,12 @@ describe ProductsController do
 	
 	describe 'GET #new' do
 		it "assigns a new product to @product" do
-			product = Product.create
+			product = FactoryGirl.create(:product)
 			get :new, id: product
 			expect(assigns(:product)).to be_a_new(Product)
 		end
 		it "renders the :new template" do
-			product = Product.create
+			product = FactoryGirl.create(:product)
 			get :new, id: product
 			expect(response).to render_template :new
 		end
@@ -48,77 +49,77 @@ describe ProductsController do
 
 	describe 'GET #edit' do
 		it "assigns the requested product to @product" do
-			product = Product.create
+			product = FactoryGirl.create(:product)
 			get :edit, id: product
 			expect(assigns(:product)).to eq product
 		end
 		it "renders the :edit template" do
-			product = Product.create
+			product = FactoryGirl.create(:product)
 			get :edit, id: product
 			expect(response).to render_template :edit
 		end
 	end
 
-		
 	describe "POST #create" do
 		context "with valid attributes" do
 			it "saves the new product in the database" do
-				product = Product.create
+				product = FactoryGirl.create(:product)
 				post :create, {id: @product.id, product: {name: 'test', description: 'test', image_url: 'test'}}
-				expect{ Product.create }.to change{ Product.count }.by(1)
+				expect{ FactoryGirl.create(:product) }.to change{ Product.count }.by(1)
 			end
 			it "redirects to products#show" do
-				product = Product.create
-				post :create, {id: @product.id, product: {name: 'test', description: 'test', image_url: 'test'}}
-				expect(response).to redirect_to ('http://test.host/products/2')
+				post :create, {product: {name: 'test', description: 'test', image_url: 'test'}}
+				expect(response).to redirect_to Product.last
 			end
 		end
-		# context "with invalid attributes" do
-		# 	it "does not save the new product in the database" do
-		# 		product = Product.create
-		# 		post :create, {id: @product.errors, product: {name: 'test', description: 'test', image_url: 'test'}}
-		# 		expect{ Product.create }.not_to change{ Product.count }
-		# 	end
-		# 	it "re-renders the :new template" do
-		# 		product = Product.create
-		# 		post :create, {id: @product.errors, product: {name: 'test', description: 'test', image_url: 'test'}}
-		# 		expect(response).to render_template :new
-		# 	end 
-		# end
+		context "with invalid attributes" do
+			it "does not save the new product in the database" do
+				
+				expect{ 
+					post :create, {product: {description: 'test'}}
+				 }.not_to change{ Product.count }
+			end
+			it "re-renders the :new template" do
+				post :create, {product: {description: 'test'}}
+				expect(response).to render_template :new
+			end 
+		end
 	end
 
-				
-	# describe 'PATCH #update' do
-	# 	context "with valid attributes" do
-	# 		it "updates the product in the database" do
-	# 			product = Product.create
-	# 			patch :update, id: product
-	# 			expect(assigns(:product)).to eq product
-	# 		end
-	# 		it "redirects to the product" 
-	# 			product = Product.create
-	# 			patch :update, id: product
-	# 			expect(response).to redirect_to @product
-	# 		end
-	# 	# context "with invalid attributes" do 
-	# 	# 	it "does not update the product" 
-	# 	# 	it "re-renders the :edit template"
-	# 	# 	end 
-	# 	# 	end
-	# end
 
-						
+	describe 'PATCH #update' do
+		context "with valid attributes" do
+			it "updates the product in the database" do
+				product = FactoryGirl.create(:product)
+				patch :update, id: product, product: {name: 'test'}
+				expect(assigns(:product)).to eq product
+			end
+			it "redirects to the product" do
+			product = FactoryGirl.create(:product)
+			patch :update, id: product, product: {name: 'test'}
+			expect(response).to redirect_to product
+			end
+		end
+		# # context "with invalid attributes" do 
+		# # 	it "does not update the product" 
+		# # 	it "re-renders the :edit template" do
+		# # 	end 
+		# 	end
+	end
+
+
 	describe 'DELETE #destroy' do
 		it "deletes the product from the database" do
-			product = Product.create
-			delete :destroy, id: product
-			expect{ Product.create }.to change{ Product.count }.by(1)
+			product = FactoryGirl.create(:product)
+			expect{ 
+				delete :destroy, id: product 
+				}.to change{ Product.count }.by(-1)
+			end
+			it "redirects to users#index" do
+				product = FactoryGirl.create(:product)
+				delete :destroy, id: product
+				expect(response).to redirect_to products_url
+			end 
 		end
-		it "redirects to users#index" do
-			product = Product.create
-			delete :destroy, id: product
-			expect(response).to redirect_to users_url
-		end 
-	end
 
-end
+	end
