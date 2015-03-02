7a876a2ba7de460615243357074b537508a5cc21
Jennifer Duvall <jduvall330@gmail.com>
2015-03-02 12:07:58 -0500

    added ajax to stripe checkout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,8 @@
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require_tree .
+$(document).ready ->
+  $("#new_payment").on("ajax:success", (e, data, status, xhr) ->
+    $("#new_payment").append xhr.responseText
+  ).on "ajax:error", (e, xhr, status, error) ->
+    $("#new_payment").append "<p>ERROR</p>"
