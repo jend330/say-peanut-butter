ff089de1ecbde7cb3c2c15ac52e05568436c51b9
Jennifer Duvall <jduvall330@gmail.com>
2015-03-05 19:58:20 -0500

    edits

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    @products = Product.search(params[:search])
   end
 
   # GET /products/1
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ActiveRecord::Base
 	validates :name, presence:true
 	attr_accessible :name, :description, :price, :image_url
+
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+		else
+			find(:all)
+		end
+	end
+
+	def price_in_cents
+		price * 100
+	end
 end
